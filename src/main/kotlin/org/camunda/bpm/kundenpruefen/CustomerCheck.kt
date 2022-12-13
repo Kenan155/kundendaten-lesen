4b2a5370df752fd88a1799075228e3026bb16844
wual1018 <alexw01@gmx.de>
2022-12-13 18:22:02 +0100

    CustomerCheck.kt überarbeitet.

--- src/main/kotlin/org/camunda/bpm/kundenpruefen/CustomerCheck.kt
+++ src/main/kotlin/org/camunda/bpm/kundenpruefen/CustomerCheck.kt
@@ -12,17 +12,36 @@ import com.fasterxml.jackson.module.kotlin.readValue
 
 object CustomerCheck {
     private val LOGGER = Logger.getLogger(CustomerCheck::class.java.name)
+
+    /**
+     * Basis-URL der Camunda-Schnittstelle.
+     */
+    private const val BASE_URL = "http://localhost:8080/engine-rest"
+
+    /**
+     * Task Topic auf das Subscribed wird, wie im Prozess angegeben
+     */
+    private const val SUBSCRIBE_TOPIC = "request-senden"
+
+    /**
+     * Die URL der Customer Datenbank
+     */
+    private const val DB_URL = "http://localhost:3000/customers"
     @JvmStatic
     fun main(args: Array<String>) {
         val client: ExternalTaskClient = ExternalTaskClient.create()
-            .baseUrl("http://localhost:8080/engine-rest")
+            .baseUrl(BASE_URL)
             .asyncResponseTimeout(10000) // long polling timeout
             .build()
 
         // subscribe to an external task topic as specified in the process
-        client.subscribe("request-senden")
+        client.subscribe(SUBSCRIBE_TOPIC)
             .lockDuration(1000) // the default lock duration is 20 seconds, but you can override this
             .handler { externalTask, externalTaskService ->
+
+                /**
+                 * Speichern der Daten aus dem Prozess in Variablen
+                 */
                 val prename: String = externalTask.getVariable("vorname")
                 val surname: String = externalTask.getVariable("nachname")
                 val email: String = externalTask.getVariable("email")
@@ -37,7 +56,11 @@ object CustomerCheck {
                 LOGGER.info("Die gewünschte Kreditsumme ist: $creditsum")
                 LOGGER.info("Die gewünschte Laufzeit ist: $duration")
 
+                /**
+                 * Erzeugen eines Customer-Objekts aus den eingegangenen Kundendaten
+                 */
                 val customerForChecking = Customer(
+                    id = null,
                     prename = prename,
                     surname = surname,
                     email = email,
@@ -46,27 +69,47 @@ object CustomerCheck {
                     duration = duration,
                 )
 
-                val fclient = HttpClient.newHttpClient()
+                /**
+                 * Erzeugen des Http Clients um Requests an die Datenbank senden zu können.
+                 */
+                val customerDbClient = HttpClient.newHttpClient()
 
+                /**
+                 * Bauen des Requests an die Datenbank.
+                 */
                 val request = HttpRequest.newBuilder()
-                    .uri(URI("http://localhost:3000/customers"))
+                    .uri(URI(DB_URL))
                     .GET()
                     .build()
 
-                val response  = fclient.send(request, BodyHandlers.ofString())
+                /**
+                 * Abschicken des Requests an die Datenbank und abspeichern der Antwort.
+                 */
+                val response  = customerDbClient.send(request, BodyHandlers.ofString())
 
-                println(response.body().toString())
-
+                /**
+                 * Bauen eines Mapper-Objekts um die JSON Daten aus der DB auf Objekte zu mappen.
+                 */
                 val mapper = jacksonObjectMapper()
 
+                /**
+                 * JSON Daten aus dem Response Body extrahieren und eine Liste an Customer-Objekten erzeugen.
+                 */
                 val json = response.body()
                 val customers = mapper.readValue<List<Customer>>(json)
 
-                customers.forEach { println(it.toString()) }
 
-                // it == ... entspricht it.equals(...)
+                /**
+                 * Customer-Objekte aus der DB mit dem Customer aus dem Prozess
+                 *
+                 * it == ... entspricht it.equals(...)
+                 */
                 val result = customers.filter { it == customerForChecking }
 
+                /**
+                 * Wenn der Customer in der DB vorhanden ist, werden die zusätzlichen Daten aus der DB zurückgegeben.
+                 * Andernfalls wird nichts zurückgegeben.
+                 */
                 if (result.isEmpty()) {
                     println("Mitarbeiter Melden")
                 } else
@@ -77,3 +120,4 @@ object CustomerCheck {
             .open()
     }
 }
+
